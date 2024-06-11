d4377d159bb3e6675dc74456495781c4122d3eff
Afonso Jansen de Mello Farias <afonsojansen@gmail.com>
2024-06-11 13:31:47 -0300

    parte 2

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        perfil.main(args);
+        estudos.main(args);
+        soma.main(args);
+        subtracao.main(args);
+    }
+}
